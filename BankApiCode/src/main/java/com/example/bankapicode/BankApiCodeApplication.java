52d8aeb13443c161929b17cafd34473049b55220
RaphaCosil <Raphael.Cosil@gmail.com>
2024-05-23 08:04:21 -0300

    Commiting project

---
+++ BankApiCode/src/main/java/com/example/bankapicode/BankApiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankapicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApiCodeApplication.class, args);
+    }
+
+}
---
+++ BankApiCode/src/test/java/com/example/bankapicode/BankApiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankapicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
